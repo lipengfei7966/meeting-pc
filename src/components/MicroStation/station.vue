d2d76637d3b6524c34cbd5aa44e3e8eff8d435df
hekaize <1982490443@qq.com>
2022-09-02 13:59:03 +0800

    微站

---
+++ src/components/MicroStation/station.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <el-button class="btn" type="success" @click="handelClick">选择模板</el-button>
+    </div>
+    <ul :class="isPc? isPc : isMo">
+      <li>模板一</li>
+      <li>模板二</li>
+      <li>模板三</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'station',
+  components: {},
+  data() {
+    return {
+      isPc: true
+    }
+  },
+  methods: {
+    handelClick() {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.isPc {
+}
+.isMo {
+}
+ul {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  li {
+    width: 30%;
+    min-height: 600px;
+    box-shadow: 0 2px 12px 0 lightgray;
+    padding: 5px 10px;
+  }
+  li:hover {
+    box-shadow: 0 2px 22px 0 lightgray;
+  }
+}
+.btn{
+  float: right;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/views/frame/website/stencil.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="contents">
+     <station ref="station" />
+  </div>
+</template>        
+<script>
+import station from '@/components/MicroStation/station'
+export default {
+  name: 'stencilManagement',
+  components: {
+    station
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+  .contents{
+    width: 100%;
+    height: 100%;
+    min-height: 600px;
+    padding: 10px;
+    // border: 1px solid red;
+  }
+</style>
