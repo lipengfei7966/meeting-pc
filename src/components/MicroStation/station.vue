dde75675da64d820c468a0738813572590227ed4
hekaize <1982490443@qq.com>
2022-09-03 17:34:02 +0800

    no message

--- src/components/MicroStation/station.vue
+++ src/components/MicroStation/station.vue
@@ -1,37 +1,29 @@
 <template>
   <div>
-    <div :class="!isPc?'place':''" style="margin-right:10%; margin-top: 10px; float: right;">
-      <el-button class="btn" type="success" @click="handelClick">选择模板</el-button>
-      <el-button v-if="!isPc" class="btn" type="success" @click="handelEdit">重新选择模板</el-button>
-      <el-button v-if="!isPc" class="btn" type="success" @click="handelAdd">新增模块</el-button>
+    <div :class="!isPc ? 'place' : ''" style="margin-right: 10%; margin-top: 10px; float: right">
+      <el-button v-if="isPc" class="btn" type="success" @click="handelClick">选择模板</el-button>
+      <el-button v-if="!isPc" style="margin-right: -40px" class="btn" type="success" @click="handelEdit">重新选择模板</el-button>
+      <!-- <el-button v-if="!isPc" class="btn" type="success" @click="handelAdd">新增模块</el-button> -->
     </div>
-    <ul :class="isPc? 'isPc' : 'isMo'">
+    <ul :class="isPc ? 'isPc' : 'isMo'">
       <li @click="handel(1)" :class="pitchOn ? 'template_one' : ''">
-        <templateOne></templateOne>
+        <templateOne @feature="feature" ref="templateOne"></templateOne>
       </li>
       <!-- <li>模板二</li>
       <li>模板三</li>
       <li>模板四</li> -->
-      <li style="font-size: 20px;
-    color: lightgray;
-    text-align: center;" @click="handel()" >
-    <span style="position: relative;
-    top: 40%;">
-      <span>更多模板 敬请期待 </span>
-        <i class="el-icon-more-outline"></i>
-    </span>
-        
-        </li>
-        <li style="font-size: 20px;
-    color: lightgray;
-    text-align: center;" @click="handel()" >
-    <span style="position: relative;
-    top: 40%;">
-      <span>更多模板 敬请期待 </span>
-        <i class="el-icon-more-outline"></i>
-    </span>
-        
-        </li>
+      <li v-if="isPc" style="font-size: 20px; color: lightgray; text-align: center" @click="handel()">
+        <span style="position: relative; top: 40%">
+          <span>更多模板 敬请期待 </span>
+          <i class="el-icon-more-outline"></i>
+        </span>
+      </li>
+      <li v-if="isPc" style="font-size: 20px; color: lightgray; text-align: center" @click="handel()">
+        <span style="position: relative; top: 40%">
+          <span>更多模板 敬请期待 </span>
+          <i class="el-icon-more-outline"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -47,7 +39,8 @@ export default {
     return {
       isPc: true,
       pitchOn: false,
-      templateVal: ''
+      templateVal: '',
+      sonVal: []
     }
   },
   methods: {
@@ -69,12 +62,34 @@ export default {
       }
     },
     handelAdd() {},
-    handelEdit() {},
+    handelEdit() {
+      this.$message('暂无更多模板！')
+    },
     handel(val) {
       this.pitchOn = !this.pitchOn
       //       debugger
       this.templateVal = val
       console.log(val)
+    },
+    feature(val) {
+      //       debugger
+      this.sonVal = val
+      this.$emit('featureVal', this.sonVal)
+      console.log(val)
+    },
+    watchVal(val) {
+      //       debugger
+      if (val == 1) {
+        this.$refs.templateOne.watchVal(val)
+      } else if (val == 2) {
+        this.$refs.templateOne.watchVal(val)
+      } else if (val == 3) {
+        this.$refs.templateOne.watchVal(val)
+      }
+      console.log(val)
+    },
+    colorVal(val) {
+      this.$refs.templateOne.colorVal(val)
     }
   }
 }
@@ -111,7 +126,7 @@ ul {
 }
 .btn {
   //   float: right;
-  margin-bottom: 20px;
+  margin-bottom: 15px;
 }
 .place {
   margin-right: 2%;
