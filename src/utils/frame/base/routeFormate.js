72f74d019160b6328b95bfe5532da8f1944286ac
hekaize <1982490443@qq.com>
2022-09-03 17:52:24 +0800

    代码找回

--- src/utils/frame/base/routeFormate.js
+++ src/utils/frame/base/routeFormate.js
@@ -2,13 +2,15 @@ import { Notification } from 'element-ui'
 import { notifyError } from '@/utils/frame/base/notifyParams'
 import Layout from '@/views/frame/layout/Layout'
 
-export const formatRoutes = (aMenu) => {
-  const aRouter = [{
-    path: '',
-    component: Layout,
-    hidden: false,
-    children: []
-  }]
+export const formatRoutes = aMenu => {
+  const aRouter = [
+    {
+      path: '',
+      component: Layout,
+      hidden: false,
+      children: []
+    }
+  ]
   if (aMenu instanceof Array) {
     // if (process.env.NODE_ENV === 'production') {
     //   aMenu.forEach(oMenu => {
@@ -44,14 +46,7 @@ export const formatRoutes = (aMenu) => {
     // } else {
     aMenu.forEach(oMenu => {
       if (oMenu.component) {
-        const {
-          name,
-          path,
-          component,
-          meta,
-          iframeFlag,
-          reuseFlag
-        } = oMenu
+        const { name, path, component, meta, iframeFlag, reuseFlag } = oMenu
         let oRouter = {}
         meta.title = name
 
@@ -63,46 +58,69 @@ export const formatRoutes = (aMenu) => {
           component(resolve) {
             if (component.includes('views/frame/base/')) {
               require([`@/views/frame/base/${component.split('views/frame/base/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/frame/dashboard/')) {
               require([`@/views/frame/dashboard/${component.split('views/frame/dashboard/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/frame/iframe/')) {
               require([`@/views/frame/iframe/${component.split('views/frame/iframe/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/frame/layout/')) {
               require([`@/views/frame/layout/${component.split('views/frame/layout/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/frame/login/')) {
               require([`@/views/frame/login/${component.split('views/frame/login/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/common/')) {
               require([`@/views/common/${component.split('views/common/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
             } else if (component.includes('views/frame/sys/')) {
               require([`@/views/frame/sys/${component.split('views/frame/sys/')[1]}.vue`], resolve, e => {
-                Notification(notifyError({
-                  msg: `${component}找不到文件`
-                }))
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
               })
+            } else if (component.includes('views/frame/website/')) {
+              require([`@/views/frame/website/${component.split('views/frame/website/')[1]}.vue`], resolve, e => {
+                Notification(
+                  notifyError({
+                    msg: `${component}找不到文件`
+                  })
+                )
+              })
+              // 0902
             } else if (component.includes('views/frame/website/')) {
               require([`@/views/frame/website/${component.split('views/frame/website/')[1]}.vue`], resolve, e => {
                 Notification(
@@ -115,9 +133,11 @@ export const formatRoutes = (aMenu) => {
               if (process.env.NODE_ENV === 'development') {
                 if (component.includes('views/example/')) {
                   require([`@/views/example/${component.split('views/example/')[1]}.vue`], resolve, e => {
-                    Notification(notifyError({
-                      msg: `${component}找不到文件`
-                    }))
+                    Notification(
+                      notifyError({
+                        msg: `${component}找不到文件`
+                      })
+                    )
                   })
                 }
               }
