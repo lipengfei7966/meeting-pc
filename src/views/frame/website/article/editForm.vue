b59de5801fbbf1db7dea3cfb27f7ee3784fce6dd
武俊杰 <junjie.wu2@ctg.cn>
2022-09-08 18:21:55 +0800

    文章菜单：去掉上传图片

--- src/views/frame/website/article/editForm.vue
+++ src/views/frame/website/article/editForm.vue
@@ -89,18 +89,18 @@ export default {
                   clearable: true,
                   cols: 2
                 }
-              },
-              {
-                label: 'website.article.edit.articlePic',
-                prop: 'articlePic',
-                element: 'image-upload',
-                attrs: {
-                  imageData: {
-                    // tenantCode: this.param.tenantCode
-                  },
-                  disabled: true,
-                  cols: 4
-                }
+              // },
+              // {
+              //   label: 'website.article.edit.articlePic',
+              //   prop: 'articlePic',
+              //   element: 'image-upload',
+              //   attrs: {
+              //     imageData: {
+              //       // tenantCode: this.param.tenantCode
+              //     },
+              //     disabled: true,
+              //     cols: 4
+              //   }
               }
             ]
           }
