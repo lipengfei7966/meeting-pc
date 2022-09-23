b40bd1392ee0034ef8bfb7950a08def527e600e4
武俊杰 <junjie.wu2@ctg.cn>
2022-09-23 10:49:29 +0000

    Update set.vue

--- src/views/frame/website/register/signupSignin/set.vue
+++ src/views/frame/website/register/signupSignin/set.vue
@@ -300,7 +300,7 @@ export default {
         this.$notify(notifyInfo({ msg: '无法获取场景code' }));
         return
       }
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', 
+      this.$confirm('确认删除?', '提示', 
       { confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning' }).then(() => 
       { 
         request({
