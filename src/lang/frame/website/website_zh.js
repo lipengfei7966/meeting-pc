e2d16a4b43a3eb19d0a41bf4f8567d871db70b74
ligang <546188950@qq.com>
2022-09-23 09:10:46 +0800

    增加表单管理功能

--- src/lang/frame/website/website_zh.js
+++ src/lang/frame/website/website_zh.js
@@ -161,6 +161,30 @@ export default {
         sceneName: '场景名称',
         createDate: '报名时间'
       }
+    },
+    signupContactCol: {
+      title: '表单管理',
+      query: {
+        name: '姓名',
+        eventCode: '会议名称'
+      },
+      list: {
+        mapCode: '名称',
+        mapName: '显示名称',
+        mapComp: '组件类型',
+        mapRequired: '必填',
+        mapType: '启用',
+        mapSort: '顺序'
+      },
+      edit: {
+        eventCode: '会议名称',
+        mapCode: '名称',
+        mapName: '显示名称',
+        mapComp: '组件类型',
+        mapType: '启用',
+        mapRequired: '必填',
+        mapSort: '顺序'
+      }
     }
   }
 }
