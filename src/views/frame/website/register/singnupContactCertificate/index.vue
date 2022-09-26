d4ce90b36195ab952b451328b291f6249487fc3d
高书杰 <623865073@qq.com>
2022-09-26 16:14:20 +0800

    修改回显

--- src/views/frame/website/register/singnupContactCertificate/index.vue
+++ src/views/frame/website/register/singnupContactCertificate/index.vue
@@ -98,7 +98,14 @@ export default {
             type: 'dialog',
             component: () => import('./edit.vue'),
             getParam: () => {
-              return this.$refs.bsTable.currentRow.code
+              return this.$refs.bsTable.currentRow[0]
+            },
+            validate: () => {
+              if (!this.$refs.bsTable.currentRow || this.$refs.bsTable.currentRow.length!=1) {
+                return false
+              }else{
+                return true
+              }
             }
           },
           {
