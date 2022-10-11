d4b1e62989b2281ce20bcdd9f948e5afd3d8b157
hekaize <1982490443@qq.com>
2022-10-11 15:25:58 +0800

    优化展示

--- src/components/MicroStation/template_one.vue
+++ src/components/MicroStation/template_one.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contents" :style="{ backgroundImage: 'url(' + loginBg + ')' }">
     <div class="block">
-      <el-carousel trigger="click" height="280px" indicator-position="none">
+      <el-carousel trigger="click" height="230px" indicator-position="none">
         <el-carousel-item v-for="(item, index) in slideshow" :key="index">
           <img :src="`${item.url}`" alt="" style="width: 100%" />
           <!-- <h2>{{ index }}</h2> -->
@@ -234,7 +234,7 @@ export default {
   margin: 0 15px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: center;
   //   align-items: center;
   li {
     text-align: center;
