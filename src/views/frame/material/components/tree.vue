b64b53ea2a8bc01bbfcf761d5d2b41ca24550bf5
hekaize <1982490443@qq.com>
2022-10-12 18:11:12 +0800

    前端素材管理修改

--- src/views/frame/material/components/tree.vue
+++ src/views/frame/material/components/tree.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-dropdown>
+            <span class="el-dropdown-link"><i class="el-icon-arrow-down el-icon--right"></i> </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click="addWork(node, data)"><span @click=";(dialogVisible = true), addWork(node, data)">新建文件夹</span></el-dropdown-item>
+              <el-dropdown-item @click="editWork(node, data)"><span @click=";(dialogVisible = true), editWork(node, data)">编辑文件夹</span></el-dropdown-item>
+              <el-dropdown-item @click="delWork(node, data)"><span @click="delWork(node, data)">删除文件夹</span></el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </span>
+      </span>
+    </el-tree>
+    <!-- 弹窗 -->
+    <el-dialog :title="title_" :visible.sync="dialogVisible" width="500px" :before-close="handleClose">
+      <span style="font-size: 14px; color: black">文件夹名称：</span>
+      <el-input style="width: 80%" @blur="blur_" v-model="workName" placeholder="请输入文件夹名称："></el-input>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="present_">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,16 +91,44 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'label'
-      }
+      },
+      dialogVisible: false,
+      title_: '新建文件夹'
     }
   },
   methods: {
     handleNodeClick(data) {
       console.log(data)
+    },
+    addWork(node, data) {
+      this.title_ = '新建文件夹'
+      console.log(node, data, 'hkz')
+    },
+    editWork(node, data) {
+      this.title_ = '编辑文件夹'
+      console.log(node, data, 'hkz')
+    },
+    delWork(node, data) {
+      console.log(node, data, 'hkz')
+    },
+    handleClose(done) {
+      done()
+    },
+    // 提交
+    present_() {
+      console.log('提交')
+      this.dialogVisible = false
     }
   }
 }
 </script>
 
 <style lang="scss">
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
 </style>
--- src/views/frame/material/index.vue
+++ src/views/frame/material/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="material">
     <el-card class="box-card content_one">
-      <tree />
+      <div>
+        <tree />
+      </div>
     </el-card>
     <el-card class="box-card content_two">
       <div class="search">
@@ -66,6 +68,36 @@
           <el-image style="width: 100%; height: 65%" src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" :preview-src-list="srcList"> </el-image>
           <el-button type="text">详情</el-button>
         </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" :src="url" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" :src="url" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" :src="url" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
+        <li class="resource">
+          <p>图片名称</p>
+          <el-image style="width: 100%; height: 65%" src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" :preview-src-list="srcList"> </el-image>
+          <el-button type="text">详情</el-button>
+        </li>
         <!-- <li class="resource">视频</li> -->
       </ul>
     </el-card>
@@ -84,7 +116,10 @@
             <p>尺寸：<span class="sp">1920*1000</span></p>
             <p>上传时间：<span class="sp">2021-02-19</span></p>
             <p>所在文件夹：<span class="sp">PC轮播图</span></p>
-            <p>文件链接：<span class="sp">http://123.123.com</span></p>
+            <p>
+              文件链接：<span class="sp">{{ aaa | commentEllipsis(aaa) }}</span>
+              <el-button style="font-size: 12px; margin-left: 5px" type="text" size="small" @click="copyUrl">复制链接</el-button>
+            </p>
             <div class="btn">
               <el-button style="margin-right: 2vw">替换文件</el-button>
               <el-button type="primary">下载</el-button>
@@ -114,16 +149,41 @@ export default {
       leixingOptions: [{ name: '内容一' }, { name: '内容二' }],
       paixuOptions: [{ name: '内容一' }, { name: '内容二' }],
       url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-      srcList: ['https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg']
+      srcList: ['https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'],
+      aaa: 'https://www.csdn.net/tags/MtTaAg0sMzMwNDEtYmxvZwO0O0OO0O0O.html'
     }
   },
   methods: {
+    // 复制文件链接
+    copyUrl(item, type) {
+      var copyTest = this.aaa
+      var inputTest = document.createElement('input')
+      inputTest.value = copyTest
+      document.body.appendChild(inputTest)
+      inputTest.select()
+      document.execCommand('Copy')
+      inputTest.className = 'oInput'
+      inputTest.style.display = 'none'
+      this.$message.success('复制成功')
+    },
     // 顶部搜索
     searchClick() {
       console.log('搜索', this.fileSearch)
     },
     blur_() {
       console.log('失焦了 调保存接口吧')
+    }
+  },
+  filters: {
+    // 过滤
+    commentEllipsis(value) {
+      if (value.length != undefined) {
+        if (value.length > 15) {
+          return value.slice(0, 14) + '...'
+        } else {
+          return value
+        }
+      }
     }
   }
 }
@@ -136,7 +196,7 @@ export default {
   display: flex;
   justify-content: space-between;
   div {
-    // border: 1px solid lightslategrey;
+    // 暂存
   }
 }
 .content_one {
@@ -157,14 +217,13 @@ export default {
 .search {
   width: 100%;
   height: 5%;
-  // border: 1px solid red;
 }
 .content_ {
   width: 100%;
+  // hkz
   height: 85vh;
   text-align: center;
   overflow-y: auto;
-  // border: 1px solid green;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -173,11 +232,14 @@ export default {
     height: 200px;
     margin-top: 10px;
     margin-right: 15px;
-    // border: 1px solid lightblue;
     box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
     p {
       line-height: 30px;
     }
+  }
+  .resource :hover {
+    // top: 2px;
+    // animation: move 0.7s linear infinite;
   }
 }
 .el-input-group__append {
@@ -187,13 +249,11 @@ export default {
 .set_one {
   width: 100%;
   height: 30vh;
-  // border: 1px solid red;
 }
 .set_two {
   margin-top: 5vh;
   width: 100%;
   height: 50vh;
-  // border: 1px solid red;
 }
 .particulars {
   p {
