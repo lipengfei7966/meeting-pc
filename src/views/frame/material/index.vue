cbc954a3aa17054cc69fe8bcb5e004aca4710c7e
hekaize <1982490443@qq.com>
2022-10-12 19:05:15 +0800

    no message

--- src/views/frame/material/index.vue
+++ src/views/frame/material/index.vue
@@ -232,7 +232,7 @@ export default {
     width: 23.5%;
     height: 40%;
     margin-top: 10px;
-    margin-right: 10px;
+    margin-right: 1%;
     box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
     p {
       line-height: 30px;
