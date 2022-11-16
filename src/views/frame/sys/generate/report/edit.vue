5419759d9ae6a49dc2eb2124f0a3fbf4c7edba22
李成 <cheng.li6@ctg.cn>
2022-11-16 19:34:51 +0800

    🐞 fix(template): 更多查询条件支持

--- src/views/frame/sys/generate/report/edit.vue
+++ src/views/frame/sys/generate/report/edit.vue
@@ -152,11 +152,11 @@
                   <el-checkbox v-model='page.codeDataJson.mainData.table.showSummary'></el-checkbox>
                 </el-form-item>
               </el-col>
-              <!-- <el-col :span="8">
+              <el-col :span="8">
                 <el-form-item label="是否驼峰转成大写下划线" prop='camelToUnderlineFlg'>
                   <el-checkbox v-model='page.codeDataJson.form.listQuery.camelToUnderlineFlg'></el-checkbox>
                 </el-form-item>
-              </el-col> -->
+              </el-col>
             </el-row>
           </el-form>
         </header>
