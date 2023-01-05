2b055f6d4d73a016de99caabca5d3405a8a7fe59
刘馥铭 <liufuming050@chinasoftinc.com>
2023-01-05 18:27:08 +0800

    '分活动'

---
+++ src/lang/frame/service/subActivities/sub_en.js
@@ -0,0 +1,52 @@
+export default {
+  sub: {
+    meetingName: 'Meeting name', //  // 会议名称
+    askSelection: 'Whether participants can participate in multiple sub activities', // 参会人是否可参加多项分活动
+    unlimited: 'unlimited', // 不限制
+    optionalMaximum: 'One person can participate at most', // 单人最多参与
+    second: 'second', // 次
+    subActivityName: 'Sub activity name', // 分活动名称
+    activityTime: 'Activity time', // 活动时间
+    activityStatus: 'Activity status', // 活动状态
+    enableStatus: 'Enable status', // 启用状态
+    startDate: 'Start date', // 开始日期
+    endDate: 'End date', // 结束日期
+    to: 'to', // 至
+    btns: {
+      screen: 'Screen', // 筛选
+      newlyBuild: 'Add' // 新建
+    },
+    column: {
+      subActivityName: 'SubActivityName', // 分活动名称
+      activityTime: 'ActivityTime', // 活动时间
+      activityStatus: 'ActivityStatus', // 活动状态
+      enable: 'Enable', // 启用
+      applicantNum: 'ApplicantNumbers/toplimit', // 报名人数/上限
+      operation: 'Operation' // 操作
+    },
+    dialogTxt: {
+      unlimited: 'unlimited', // 不限
+      none: 'nil', // 无
+      newSubActivity: 'New Sub activity', // 新建分活动
+      modifySubActivity: 'Modify Sub activity', // 修改分活动
+      subActivityNamePH: 'Please enter the name of the sub activity', // 请输入分活动名称
+      participantsNumLimit: 'Limit the number of participants', // 参加人数限制
+      participantsMax: 'No more than', // 活动最多允许
+      participation: 'people are allowed to participate', // 人参与
+      subDescription: 'Sub activity description', // 分活动描述
+      subDescriptionPH: 'Please enter the sub activity description' // 请输入分活动描述
+    },
+    msg: {
+      noEnteredMsg: 'Please enter the activity name', // 请输入活动名称
+      noSelectedMsg: 'Please select the activity area', // 请选择活动区域
+      noDateMsg: 'Please select a date', // 请选择日期
+      noChooseMsg: 'Please select at least one activity nature', // 请至少选择一个活动性质
+      noResourceMsg: 'Please select an activity resource', // 请选择活动资源
+      noDescribeMsg: 'Please fill in the sub activity description', // 请填写分活动描述
+      nolimitMsg: 'Please fill in the number of participants', // 请填参加人数限制
+      deleteDataMsg: 'Delete current data?', // 是否删除当前数据？
+      manageDelMsg: 'Delete by activity management', // 分活动管理删除
+      cancelDelMsg: 'Deletion cancelled' // 已取消删除
+    }
+  }
+}
