87e8f8dd602a0ce798067f5dbff894c847c7f3bc
“guoyaxu” <fruitlet5@163.com>
2023-01-10 15:55:48 +0800

    系统管理模块国际化

--- src/lang/frame/service/informManage/msg_en.js
+++ src/lang/frame/service/informManage/msg_en.js
@@ -127,6 +127,16 @@ export default {
       resendSuccess: 'Resend success!',
       confirmCancel: 'Confirm cancel?',
       cancelSuccess: 'Cancel success!',
+      sendNum: 'Quantity Sent',
+      tenantCode: 'tenant',
+      msgTaskId: 'Task ID',
+      msgProcessId: 'Process ID',
+      recipientAccount: 'Receiving Account',
+      msgClassType: 'Message Level',
+      msgTitle: 'Message Headers',
+      msgContent: 'Message Content',
+      msgExtend: 'Message Extension',
+      tenantName: 'Lessee',
       required: {}
     },
     webmsg: {
