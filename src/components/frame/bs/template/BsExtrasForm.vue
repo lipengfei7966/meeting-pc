32112dc11d580846da07391fdad58ff89c732a5d
李成 <cheng.li6@ctg.cn>
2023-01-17 18:36:09 +0800

    ✨ feat(report): 自动生成的报表页面，不需求处理多语言

--- src/components/frame/bs/template/BsExtrasForm.vue
+++ src/components/frame/bs/template/BsExtrasForm.vue
@@ -4,9 +4,8 @@
       <el-row :gutter="20" style='width:94%;' justify="space-between">
         <template v-for='(f, index) in expandStatus ?  (form.formData[0].attrs&&form.formData[0].attrs.cols) ? form.formData.slice(0,2) : form.formData.slice(0, 3) :form.formData'>
           <el-col :span="f.attrs && f.attrs.cols ? f.attrs.cols * 6 : 6" v-if='f.isShow' :key='index'>
-            <!-- {{ f }} -->
             <!-- 日期 -->
-            <el-form-item v-if='f.type === "daterange" ||f.type === "datetimerange"' :label="$t(`count.${ $route.meta.title.replace('m', '' )}.form.${f.prop}`)" :prop='f.prop'>
+            <el-form-item v-if='f.type === "daterange" ||f.type === "datetimerange"' :label="$t(f.label)" :prop='f.prop'>
               <el-date-picker v-model="form.listQuery.data[f.prop]" :picker-options="f['picker-options']" type="daterange" range-separator="~" start-placeholder="" end-placeholder="" v-bind='f.attrs' @change='changeDaterangeTime(f)'>
               </el-date-picker>
             </el-form-item>
@@ -59,8 +58,7 @@
               </el-row>
             </el-form-item>
             <!-- 下拉输入 -->
-            <!-- :label="$t(`count.${ $route.meta.title.replace('m', '' )}.form.${f.prop}`)" -->
-            <el-form-item v-else :label="$t(`count.${ $route.meta.title.replace('m', '' )}.form.${f.prop}`)" :prop="f.prop">
+            <el-form-item v-else :label="f.label" :prop="f.prop">
               <!-- 字典码表 -->
               <el-select v-if="f.list && dataDictList[f.list]" v-model="form.listQuery.data[f.prop]" v-bind="f.attrs" placeholder="" @change="f.event && f.event.change()">
                 <el-option v-for="item in dataDictList[f.list]" :key="item.value" :label="item.label" :value="item.value"></el-option>
@@ -97,10 +95,7 @@
             </div>
           </el-col>
           <el-col :span="6" v-if='form.moreShowFlg && addQueryConditionVisible'>
-            <span class='more-query' ref="moreQuery" @click='addQueryCondition' {{>$t('biz.btn.extend')}}&nbsp;+</span>
-          </el-col>
-        </template>
-        <el-col class="none"></el-col>
+            <span class='more-query' ref="moreQuery" @click='addQueryCondition' {{>$t('biz.btn.extend')}}&nbsp;+</span> </el-col> </template> <el-col class="none"></el-col>
       </el-row>
       <!-- 右侧搜索按钮 -->
       <el-col :span="6">
